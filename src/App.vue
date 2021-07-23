54283b5818af4876fed63fbb9d0d94f5e2c23ce3
limingzhu0916 <50943970+limingzhu0916@users.noreply.github.com>
2021-07-23 16:31:03 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,42 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view></router-view>
+    <tab-bar>
+      <tab-bar-item path="/home">
+        <img src="./assets/img/tabbar/Home.svg" alt="" slot="item-icon">
+        <img src="./assets/img/tabbar/Home_1.svg" alt="" slot="item-icon-activate">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category">
+        <img src="./assets/img/tabbar/category_1.svg" alt="" slot="item-icon">
+        <img src="./assets/img/tabbar/category.svg" alt="" slot="item-icon-activate">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart">
+        <img src="./assets/img/tabbar/chart_1.svg" alt="" slot="item-icon">
+        <img src="./assets/img/tabbar/chart.svg" alt="" slot="item-icon-activate">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile">
+        <img src="./assets/img/tabbar/my.svg" alt="" slot="item-icon">
+        <img src="./assets/img/tabbar/my_1.svg" alt="" slot="item-icon-activate">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+    </tab-bar>
   </div>
 </template>
+<script>
+import TabBar from './components/tabber/TabBer.vue'
+import TabBarItem from './components/tabber/TabBarItem.vue'
+export default {
+  name: "App",
+  components: {
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+@import "./assets/css/base.css"
 </style>
---
+++ src/components/tabber/TabBarItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActivate"><slot name="item-icon"></slot></div>
+    <div v-else><slot name="item-icon-activate"></slot></div>
+    <div :class="{activate: isActivate}"><slot name="item-text"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isActivate: false
+    }
+  },
+  props: {
+    path: String
+  },
+  methods: {
+   itemClick() {
+     return this.$router.replace(this.path)
+   }
+  }
+};
+</script>
+
+<style>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  font-size: 14px;
+  color: #6E6E6E;
+}
+.tab-bar-item img{
+  width: 30px;
+  height: 25px;
+  margin-top: 3px;
+  vertical-align: middle;
+}
+.activate{
+  color: #1C1C1C;
+}
+</style>

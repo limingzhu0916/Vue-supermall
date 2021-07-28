39f3db1e68d1dc963514f8db079e3ab958bd3eb3
limingzhu0916 <50943970+limingzhu0916@users.noreply.github.com>
2021-07-28 18:25:33 +0800

    home nav-bar

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,24 @@
 <template>
-  <div>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
     首页
   </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
 export default {
-
+  components: {
+    NavBar
+  }
 }
 </script>
 
 <style>
-
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
